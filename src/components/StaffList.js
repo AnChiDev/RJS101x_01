628cddb2b3d128c928754fd371cc8869302eb74e
An Chi <designbyanchi@gmail.com>
2021-11-30 16:47:44 +0700

    search - uncontrolled form

--- src/components/StaffList.js
+++ src/components/StaffList.js
@@ -1,37 +1,32 @@
 import React, {useState} from 'react';
-import { Card, CardImg, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardTitle, Input, Label, Form, FormGroup, Button } from 'reactstrap';
 import {Link} from 'react-router-dom';
 
-function RenderMenuItem ({staff}) {
-    return (
-        <div>
-        <Card >
+const StaffList = ({staffs, updateState}) => {
+    const menu = staffs.map((staff) => {
+        return (
+            <div className="col-6 col-md-4 col-lg-2 mb-2"  key={staff.id}>
+                    <Card >
             <Link to={`/menu/${staff.id}`} >
                 <CardImg width="100%" src={staff.image} alt={staff.name} />
                 <CardTitle className="text-center text-dark">{staff.name}</CardTitle>
                
             </Link>
-        </Card>
-        </div>
-    );
-}
-    const StaffList = (props) => {
-
-        const menu = props.staffs.map((staff) => {
-            return (
-                <div className="col-6 col-md-4 col-lg-2 mb-2"  key={staff.id}>
-                    <RenderMenuItem staff={staff} />
+            </Card>
                 </div>
             );
         });
+        const [Name, setName] = useState("");
+        const [Search, setSearch] = useState ("");
 
-        const [search, setSearch] = useState("");
-
-        const SEARCH = props.staffs.filter((staff) => {
-            if (search === "") {
-              return staff
-            } else if (staff.name.toLowerCase().includes(search.toLowerCase())){
-              return staff
+        const handleSearch =(event,Name)=>{
+            event.preventDefault(); 
+            const name = Name.value;
+            const X = staffs.filter((staff) => {
+            if (name === "") {
+              return staff;
+            } else if (staff.name.toLowerCase().includes(name.toLowerCase())){
+              return staff;
             }
           }).map((staff) => {
             return (
@@ -46,6 +41,12 @@ function RenderMenuItem ({staff}) {
               </Link>
             );
           })
+         setSearch(X);
+          Name.value ="";
+
+        };
+
+        
 
         return (
             <div className ="container" >
@@ -58,19 +59,37 @@ function RenderMenuItem ({staff}) {
                 <div className="row">
                     {menu}
                 </div>
-                        <div className="bg-light" style={{ 'width' : '300px'}}>
+                <div className =" mt-2 mb-2">
+                    <h6> Bấm vào tên nhân viên để xem thông tin.</h6>
+                </div>
+                <div className="bg-light" style={{ 'width' : '500px'}}>
+                <Form>
                 <input 
                     type="text" 
-                    placeholder="Tìm kiếm theo tên nhân viên" 
+                    placeholder="Nhập tên nhân viên cần tìm" 
+                    id="name"
+                    name="name"
                     style={{ 'width' : '300px' }}
-                    onChange={ (event) => setSearch(event.target.value) }></input>
+                    ref ={(input) => {return setName(input);}}></input>
+               <Button className ="btn btn-info" type ="submit" onClick={(event) => handleSearch(event, Name)}> Tìm </Button>
+               </Form>
+                </div>
                 <div>
-                    { search !== "" ? SEARCH : <div></div>}
-                </div>
-                </div>
-                        <div className =" mt-2 mb-2">
-                        <h6> Bấm vào tên nhân viên để xem thông tin.</h6>
-                </div>
+                  <p>
+                    {Search === null
+                      ? "* Bấm vào tên nhân viên để xem thông tin."
+                      : Search .length === 0
+                      ? ""
+                      : "* Bấm vào tên nhân viên để xem thông tin."}
+                  </p>
+               </div>      
+               <div>
+                  {Search  === null
+                    ? "*Không tìm thấy nhân viên nào"
+                    : Search.length === 0
+                    ? "*Không tìm thấy nhân viên nào"
+                    : Search}
+                </div>   
          </div> 
         );
     }
